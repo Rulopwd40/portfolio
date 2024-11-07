ea60f202b1f59f8300a66da06f0c211498475057
Rulopwd40 <132414402+Rulopwd40@users.noreply.github.com>
2024-11-07 18:53:52 -0300

    V1

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -2,18 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AnimatedbuttonComponent } from './animatedbutton/animatedbutton.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, 
     AnimatedbuttonComponent,
+    
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  
+  me= {
+    image:'me-image.jpg'
+  }
+
   frontend = [
     { name: 'Angular', imageUrl: 'angular.svg' },
     { name: 'HTML5', imageUrl: 'html5.svg' },
@@ -41,7 +46,25 @@ export class AppComponent {
     { name:'StockSalesSystem' , 
       url:'https://github.com/Rulopwd40/StockSalesSystem', 
       description:'System made for product selling,stock management and stats reports',
-      technology:'Java'}
+      technology:'Java'},
+
+    { name: 'AppointmentsManagementSystem',
+      url:'https://github.com/Rulopwd40/appms',
+      description:'System for appointments creation and management',
+      technology:'C# ASP.NET'
+    },
+    {
+      name: 'ProjectKanban',
+      url:'https://github.com/Rulopwd40/ProjectKanban',
+      description:'A Kanban with drag & drop with Devs cards and taskcards',
+      technology:'Angular'
+    }
   ]
 
+  scrollToSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
