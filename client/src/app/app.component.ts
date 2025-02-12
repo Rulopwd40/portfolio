4f216d566d886e49aca1b456bee31a754220d303
Rulopwd40 <132414402+Rulopwd40@users.noreply.github.com>
2025-02-12 15:29:06 -0300

    Corrections and additions

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AnimatedbuttonComponent } from './animatedbutton/animatedbutton.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, 
+    AnimatedbuttonComponent,
+    CommonModule
+    ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'portfolio';
+  
+  me = {
+    image: "me-image.jpg",
+  };
+
+  frontend = [
+    { name: 'Angular', imageUrl: 'angular.svg' },
+    { name: 'HTML5', imageUrl: 'html5.svg' },
+    { name: 'Typescript', imageUrl: 'ts.svg' }
+  ];
+
+  backend = [
+    { name: 'Java', imageUrl: 'java.svg' },
+    { name: 'Springboot' , imageUrl:'springboot.svg'},
+    { name: 'Hibernate', imageUrl:'hibernate.svg'},
+    { name: 'Golang', imageUrl: 'go.svg'},
+    { name: 'C#', imageUrl: 'cs.svg'},
+    { name: 'C++', imageUrl: 'c++.svg'}
+  ]
+
+  databases = [
+    { name: 'PostgreSQL', imageUrl: 'postgresql.svg'},
+    { name: 'SQLite', imageUrl: 'sqlite.svg'},
+  ]
+
+  miscellaneous = [
+    { name:'Github', imageUrl:'github.svg'}
+  ]
+
+  projects = [
+    { 
+      name: 'Forge',
+      url: 'https://github.com/Rulopwd40/Forge',
+      description: "Workouts generator (WIP)",
+      technology: 'Golang Angular'
+    },
+    { 
+      name: 'StockSalesSystem', 
+      url: 'https://github.com/Rulopwd40/StockSalesSystem', 
+      description: 'System made for product selling, stock management and stats reports',
+      technology: 'Java'
+    },
+    {
+      name: 'Delivery Management System',
+      url: 'https://github.com/gonzalo-aguero/delivery-management-system',
+      description: 'MVP where my principal role was the creation of the API, business logic with services, and JPARepositories',
+      technology: 'Java Springboot Hibernate'
+    },
+    { 
+      name: 'AppointmentsManagementSystem',
+      url: 'https://github.com/Rulopwd40/appms',
+      description: 'System for appointments creation and management',
+      technology: 'C# ASP.NET'
+    },
+    {
+      name: 'ProjectKanban',
+      url: 'https://github.com/Rulopwd40/ProjectKanban',
+      description: 'A Kanban with drag & drop with Devs cards and taskcards',
+      technology: 'Angular'
+    },
+  ]
+
+  languages = [
+    { name: 'Spanish', level: 'Native', imageUrl:'spanish.png'},
+    { name: 'English', level: 'Upper Intermediate', imageUrl:'english.svg' },
+    { name: 'German', level: 'Beginner', imageUrl:'deutsch.svg'}
+  ]
+
+  scrollToSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      console.log(section);
+      const headerOffset = 100; // Ajusta este valor según la altura de tu encabezado
+      const elementPosition = section.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.scrollY - headerOffset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
